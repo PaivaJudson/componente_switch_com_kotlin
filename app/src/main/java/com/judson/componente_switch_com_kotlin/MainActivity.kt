7e5fd2175a80df50aba508e8acb67c8db9e48b8a
Judson Paiva <judson.paiva@hotmail.com>
2021-09-24 02:02:34 +0100

    codificando o setOnChekedChangeListener

--- app/src/main/java/com/judson/componente_switch_com_kotlin/MainActivity.kt
+++ app/src/main/java/com/judson/componente_switch_com_kotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.judson.componente_switch_com_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sw_switch.setOnCheckedChangeListener { buttonView, isChecked -> isChecked
+            var ligadoOuDesligado = ""
+            if (isChecked){
+                ligadoOuDesligado = "Ligado"
+            }else{
+                ligadoOuDesligado = "Desligado"
+            }
+            txt_status.text = ligadoOuDesligado
+        }
     }
 }
